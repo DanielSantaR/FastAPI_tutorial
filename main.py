8d2ad096628026e20f8ca78c84cf58b559f75ed6
DanielSantaR <daniel.santar@udea.edu.co>
2020-05-26 22:45:39 -0500

    HTTException

--- main.py
+++ main.py
@@ -1,56 +1,80 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from starlette import status
-from typing import List, Union
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 
-class Person(BaseModel):
-    name: str
-    age: int
-    sex: str
-    profession: str
+class User(BaseModel):
+    user_name: str
+    user_surname: str
+    user_nationality: str
+    user_age: Optional[int]
 
 
-class Student(Person):
-    profession = "student"
-    student_grade: int = Field(..., ge=0, le=11)
+class UserIn(User):
+    user_password: str
 
 
-class Teacher(Person):
-    profession = "teacher"
-    teacher_courses: List[int] = Field(..., ge=0, le=11)
+class UserOut(User):
+    pass
 
 
-people = {
-    0: { 
-        "name": "juan",
-        "age": 20,
-        "sex": "male",
-        "student_grade": 10,
-        },
-    1: {
-        "name": "lucas",
-        "age": 40,
-        "sex": "male",
-        "teacher_courses": [7,8,9,10],        
+class Item(BaseModel):
+    item_name: str
+    item_description: Optional[str]
+    item_price: float
+    item_tax: Optional[float]
+
+
+users = {
+    0: {
+        'user_name': 'juan',
+        'user_surname': 'lopez',
+        'user_nationality': 'colombian',
+        'password': '1235'
     },
+    1: {
+        'user_name': 'lucas',
+        'user_surname': 'ocampo',
+        'user_nationality': 'rusian',
+        'age': 20,
+        'password': 'abcdf'
+    },
+}
+
+items = {
+    0: {
+        'item_name': 'reloj',
+        'item_description': 'reloj de oro',
+        'item_price': 2000,
+    },
+    1: {
+        'item_name': 'celular',
+        'item_price': 2000,
+        'item_tax': 0.5,
+    }
 }
 
 
 app = FastAPI()
 
-@app.get('/{person_id}', response_model=Union[Student, Teacher])
-async def get_anyone(
-    *,
-    person_id: int
-):
-    return people[person_id]
 
-@app.post('/new/{person_id}', status_code=status.HTTP_201_CREATED)
-async def new_student(
+@app.get('/user/{user_id}', response_model=UserOut)
+async def get_user_by_id(
     *,
-    person_id: int,
-    student: Student
+    user_id: int
 ):
-    people.update({person_id: {**student.dict()}})
-    return {'message': f'{student.name} successfully saved'}
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail='User not found')
+    found_user = users[user_id]
+    return UserOut(**found_user)
+
+
+@app.get('/item/{item_id}', response_model=Item)
+async def get_item_by_id(
+    *,
+    item_id: int
+):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail='Item not found')
+    found_item = items[item_id]
+    return Item(**found_item)

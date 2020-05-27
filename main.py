ab0691249ee327df5ec40a2e807b9213823448cf
DanielSantaR <daniel.santar@udea.edu.co>
2020-05-27 11:18:54 -0500

    more html methods

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Path
 from pydantic import BaseModel
-from typing import Optional
+from typing import List, Optional
 
 
 class User(BaseModel):
@@ -27,28 +25,23 @@ class Item(BaseModel):
     item_tax: Optional[float]
 
 
-class UnicornException(Exception):
-    def __init__(self, id: str):
-        self.id = id
-
-
-users = {
+users_storage = {
     0: {
         'user_name': 'juan',
         'user_surname': 'lopez',
         'user_nationality': 'colombian',
-        'password': '1235'
+        'user_password': '1235'
     },
     1: {
         'user_name': 'lucas',
         'user_surname': 'ocampo',
         'user_nationality': 'rusian',
-        'age': 20,
-        'password': 'abcdf'
+        'user_age': 20,
+        'user_password': 'abcdf'
     },
 }
 
-items = {
+items_storage = {
     0: {
         'item_name': 'reloj',
         'item_description': 'reloj de oro',
@@ -63,14 +56,6 @@ items = {
 
 
 app = FastAPI()
-
-
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=430,
-        content={'message': f'id {exc.id} not found'}
-    )
 
 
 @app.get(
@@ -88,28 +73,150 @@ async def get_user_by_id(
     Get all the information of a user
 
     - **user_name:** required
-    - **surname:** required
-    - **nationality:** required
-    - **age:** optional
+    - **user_surname:** required
+    - **user_nationality:** required
+    - **user_age:** optional
     """
-    if user_id not in users:
-        raise UnicornException(user_id)
-    found_user = users[user_id]
+    if user_id not in users_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No user found with id {user_id}"
+        )
+    found_user = users_storage[user_id]
     return UserOut(**found_user)
+
+
+@app.post(
+    '/user/add_user/{user_id}',
+    tags=['users'],
+)
+async def new_user(
+    *,
+    user_id: int = Path(..., ge=0),
+    user: UserIn,
+):
+    if user_id in users_storage:
+        raise HTTPException(
+            status_code=409, detail=f"A user with id {user_id} already exists"
+        )
+    users_storage.update({user_id: user.__dict__})
+
+    message = "User successfully saved"
+    return {'message': message}
+
+
+@app.put(
+    '/user/update_user/{user_id}',
+    tags=['users'],
+)
+async def update_user(
+    *,
+    user_id: int = Path(..., ge=0),
+    user: UserIn,
+):
+    if user_id not in users_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No user found with id {user_id}"
+        )
+    users_storage[user_id] = user.__dict__
+
+    message = "User successfully updated"
+    return {'message': message}
+
+
+@app.delete(
+    '/user/delete_user/{user_id}',
+    tags=['users'],
+)
+async def delete_user(
+    *,
+    user_id: int = Path(..., ge=0)
+):
+    if user_id not in users_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No user found with id {user_id}"
+        )
+    users_storage.pop(user_id)
+
+    message = "User successfully deleted"
+    return {'message': message}
 
 
 @app.get(
     '/item/{item_id}',
     response_model=Item,
     tags=['items'],
-    description='get all the information of an item by id',
-    deprecated=True,
 )
 async def get_item_by_id(
     *,
     item_id: int
 ):
-    if item_id not in items:
-        raise UnicornException(item_id)
-    found_item = items[item_id]
+    """
+    Get all the information of an item
+
+    - **item_name:** required
+    - **item_description:** optional
+    - **item_price:** required
+    - **item_tax:** optional
+    """
+    if item_id not in items_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No item not found with id {item_id}"
+        )
+    found_item = items_storage[item_id]
     return Item(**found_item)
+
+
+@app.post(
+    '/items/add_item/{item_id}',
+    tags=['items'],
+)
+async def new_item(
+    *,
+    item_id: int = Path(..., ge=0),
+    item: Item,
+):
+    if item_id in items_storage:
+        raise HTTPException(
+            status_code=409, detail=f"An item with id {item_id} already exists"
+        )
+    items_storage.update({item_id: item.__dict__})
+
+    message = "Item successfully saved"
+    return {'message': message}
+
+
+@app.put(
+    '/item/update_item/{item_id}',
+    tags=['items'],
+)
+async def update_item(
+    *,
+    item_id: int = Path(..., ge=0),
+    item: Item,
+):
+    if item_id not in items_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No item found with id {item_id}"
+        )
+    items_storage[item_id] = item.__dict__
+
+    message = "Item successfully updated"
+    return {'message': message}
+
+
+@app.delete(
+    '/item/delete_item/{item_id}',
+    tags=['items'],
+)
+async def delete_item(
+    *,
+    item_id: int = Path(..., ge=0)
+):
+    if item_id not in items_storage:
+        raise HTTPException(
+            status_code=404, detail=f"No user found with id {item_id}"
+        )
+    items_storage.pop(item_id)
+
+    message = "Item successfully deleted"
+    return {'message': message}

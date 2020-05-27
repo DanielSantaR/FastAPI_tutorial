8a670feba499f1d2b34b41c98270a6c638be880e
DanielSantaR <daniel.santar@udea.edu.co>
2020-05-27 07:54:54 -0500

    custom exceptions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Optional
 
@@ -23,6 +25,11 @@ class Item(BaseModel):
     item_description: Optional[str]
     item_price: float
     item_tax: Optional[float]
+
+
+class UnicornException(Exception):
+    def __init__(self, id: str):
+        self.id = id
 
 
 users = {
@@ -58,13 +65,21 @@ items = {
 app = FastAPI()
 
 
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=430,
+        content={'message': f'id {exc.id} not found'}
+    )
+
+
 @app.get('/user/{user_id}', response_model=UserOut)
 async def get_user_by_id(
     *,
     user_id: int
 ):
     if user_id not in users:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise UnicornException(user_id)
     found_user = users[user_id]
     return UserOut(**found_user)
 
@@ -75,6 +90,6 @@ async def get_item_by_id(
     item_id: int
 ):
     if item_id not in items:
-        raise HTTPException(status_code=404, detail='Item not found')
+        raise UnicornException(item_id)
     found_item = items[item_id]
     return Item(**found_item)
